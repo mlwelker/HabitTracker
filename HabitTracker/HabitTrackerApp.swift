35b775f4017952565d5ef276a10f07b121528129
mlwelker <nutria.watts.0g@icloud.com>
2023-08-21 23:36:41 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Michael Welker on 2023-08-21.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
